a9cfb6a4b526c1ad58ea4d4e24d4a503b3ee1287
Lalit527 <lalit.slg007@gmail.com>
2018-04-23 10:56:30 +0530

    Added missing element

--- Sequence1/Array/missing-element.py
+++ Sequence1/Array/missing-element.py
@@ -10,4 +10,22 @@ def findMissing(arr1, arr2):
 
 arr1 = [5, 7, 8]
 arr2 = [5,8]
-print(findMissing(arr1, arr2))
+#print(findMissing(arr1, arr2))
+
+# finding the missing element
+from collections import defaultdict
+def findMissing2(arr1, arr2):
+    d = defaultdict(int)
+    for num in arr2:
+        d[num] += 1
+    for num in arr1:
+        if d[num] == 0:
+            return num
+        else:
+            d[num] -= 1
+
+arr1 = [5, 7, 8]
+arr2 = [5,8]
+print("The missing element is", findMissing2(arr1, arr2))
+
+
