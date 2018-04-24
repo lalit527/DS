18c621ec42554d63b24d6f244ae8599f9c449850
Lalit527 <lalit.slg007@gmail.com>
2018-04-24 07:08:05 +0530

    Added program for pareenthesis check

---
+++ Sequence1/Stack-Queue/balance-parenthesis.py
@@ -0,0 +1,27 @@
+def balanceParen(s):
+	if len(s) == 0:
+		return True
+	stack = []
+	opening = set('([{')
+	closing = set(')]}')
+	matches = {
+		'(' : ')',
+		'[' : ']',
+		'{' : '}'
+	}
+
+	for i in s:
+		if i in opening:
+			stack.append(1)
+		elif i in closing:
+			last = stack.pop()
+			if last != i:
+				return False
+
+	if len(stack) != 0:
+		return True
+
+	return False
+
+
+print(balanceParen('([}[[[[]]'))
