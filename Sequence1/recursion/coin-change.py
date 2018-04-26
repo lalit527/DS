fd2e72d03ebbe61e8a378b686ed9517d51c6854b
Lalit527 <lalit.slg007@gmail.com>
2018-04-26 21:20:52 +0530

    Added program for recursion

--- Sequence1/recursion/coin-change.py
+++ Sequence1/recursion/coin-change.py
@@ -1,14 +1,39 @@
-def coinChange(value, changes):
-    if value == 0:
+def coin_change(value, changes):
+    min_coins = value
+
+    if value in changes:
         return 1
-    
-    if value < 0:
-        return 0
+    else:
+        for i in [c for c in changes if c <= value]:
+            num_coins = 1 + coin_change(value - i, changes)
 
-    combination += coinChange(value, changes[])
+        if num_coins < min_coins:
+            min_coins = num_coins
 
-    return combination
+    return min_coins
 
-changes = [1, 2, 3]
-value = 4
-print(coinChange(value, changes))
+def coin_change_dyn(value, changes, results):
+    min_coins = value
+
+    if value in changes:
+        results[value] = 1
+        return 1
+    elif results[value] > 0:
+        return results[value]
+
+    else:
+        for i in [c for c in changes if c <= value]:
+            num_coins = 1 + coin_change_dyn(value - i, changes, results)
+
+        if num_coins < min_coins:
+            min_coins = num_coins
+
+            results[value] = min_coins
+
+    return min_coins
+
+
+changes = [1,5,10,25]
+value = 63
+known_results = [0]*(value+1)
+print(coin_change_dyn(value, changes, known_results))
