b0aeac4f5fe6df94c2fcbb5927af51d373853c61
Lalit527 <lalit.slg007@gmail.com>
2018-05-01 21:37:09 +0530

    Added program for graphs

---
+++ Sequence1/Graphs/graph.py
@@ -0,0 +1,51 @@
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connections = {}
+
+    def addNeighbour(self, nb, weight = 0):
+        self.connections[nb] = weight
+    
+    def getConnections(self):
+        return self.connections.keys()
+
+    def getId(self):
+        return self.id
+    
+    def getWeight(self, nb):
+        return self.connections[nb]
+
+    def __str__(self):
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connections])
+
+class Graph:
+    def __init__(self):
+        self.vertexList = {}
+        self.numVertices = 0
+
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertexList[key] = newVertex
+        return newVertex
+    
+    def getVertex(self, n):
+        if n in self.vertexList:
+            return self.vertexList[n]
+        else:
+            return None
+
+    def addEdge(self, f, t, cost = 0):
+        if f not in self.vertexList:
+            nv = self.addVertex(f)
+        if t not in self.vertexList:
+            nv = self.addVertex(t)
+
+    def getVertices(self):
+        return self.vertexList.keys()
+
+    def __iter__(self):
+        return iter(self.vertexList)
+
+    def __contains__(self, n):
+        return n in self.vertexList
