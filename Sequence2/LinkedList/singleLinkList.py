105815c211b1b60fdc1b8792ad1a4845ceb7da13
Lalit527 <lalit.slg007@gmail.com>
2018-05-05 21:57:19 +0530

    Starting of LinkList

---
+++ Sequence2/LinkedList/singleLinkList.py
@@ -0,0 +1,30 @@
+class Node:
+  def __init__(self, key):
+    self.key = key
+    self.next = None
+  
+class SingleLinkList:
+  def __init__(self):
+    self.head = None
+  
+  def insert(self, n):
+    if self.head is None:
+      self.head = Node(n)
+    else:
+      tmp = self.head
+      while tmp.next != None:
+        tmp = tmp.next
+      tmp.next = Node(n)
+
+  def printData(self):
+    tmp = self.head
+    while tmp is not None:
+      print(tmp.key)
+      tmp = tmp.next
+
+  
+s = SingleLinkList()
+arr = [1, 2, 3, 4, 5]
+for i in arr:
+  s.insert(i)
+s.printData()
