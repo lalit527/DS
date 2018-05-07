f2178ca24ad62e8a56e3cd937e23753362ff1b5c
Lalit527 <lalit.slg007@gmail.com>
2018-05-07 12:53:47 +0530

    Added program for Reversing Link list

--- Sequence2/LinkedList/reverseList.py
+++ Sequence2/LinkedList/reverseList.py
@@ -12,10 +12,28 @@ def reverseList(head):
       head = head.next
   return prev
 
+def reverseN(head, n):
+  if head is None:
+    return 
+  prev = None
+  tmp = head.next
+  count = 1
+  while n >= count and head is not None:
+    head.next = prev
+    prev = head
+    head = tmp
+    if tmp:
+      tmp = tmp.next
+    count += 1
+  if head is not None:
+    prev = prev.next
+    prev = reverseN(prev, n)
+  return prev
+
 s = SingleLinkList()
 arr = [1, 2, 3, 4, 5]
 for i in arr:
   s.insert(i)
-node = reverseList(s.head)
+node = reverseN(s.head, 2)
 s.head = node
 s.printData()
