db66c3d39bc2458e290268b26ccdb7c1ed8c56ff
Lalit527 <lalit.slg007@gmail.com>
2018-05-09 14:52:07 +0530

    Added for right tree view

--- Sequence2/Trees/treeview.py
+++ Sequence2/Trees/treeview.py
@@ -29,6 +29,35 @@ def leftView(root):
       if len(queue) == 0:
         done = True
 
+
+def rightView(root):
+  if root is None:
+    return
+  queue = deque()
+  queue.append(None)
+  queue.append(root)
+  done = False
+  while not done:
+    node = queue.popleft()
+    if node is None:
+      if len(queue) > 0:
+        node = queue.popleft()
+        print(node.data)
+        queue.append(None) 
+        if node.right is not None:
+          queue.append(node.right)
+        if node.left is not None:
+          queue.append(node.left)
+      else:
+        done = True
+    else:
+      if node is not None and node.right is not None:
+        queue.append(node.right)
+      if node is not None and node.left is not None:
+        queue.append(node.left)
+      if len(queue) == 0:
+        done = True
+
 class MaxLevel:
   def __init__(self, value):
     self.value = value
@@ -46,6 +75,20 @@ def _leftViewRec(root, level, maxlevel):
   _leftViewRec(root.left, level + 1, maxlevel)
   _leftViewRec(root.right, level + 1, maxlevel)
   return
+
+def rightViewRec(root):
+  m = MaxLevel(-1)
+  _rightViewRec(root, 0, m)
+
+def _rightViewRec(root, level, maxlevel):
+  if root is None:
+    return
+  if maxlevel.value < level:
+    print(root.data)
+    maxlevel.value = level
+  _rightViewRec(root.right, level + 1, maxlevel)
+  _rightViewRec(root.left, level + 1, maxlevel)
+  return
   
 
 t = BinaryTree()
@@ -56,4 +99,4 @@ t.insertRight(7)
 t.insertRight(9)
 t.insertLeftNode(7, 6)
 t.insertRightNode(9, 11)
-leftViewRec(t.root)
+rightView(t.root)
