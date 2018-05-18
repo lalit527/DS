97c9a112ac23552cd02812542a1efa5c86e74697
Lalit527 <lalit.slg007@gmail.com>
2018-05-18 21:39:22 +0530

    Added program for string matching naive approach

---
+++ Sequence2/String-Matching/kmp.py
@@ -0,0 +1,28 @@
+def computeLPS(pat, m, lps):
+  len = 0
+  lps[0]
+  i = 1
+  while i < m:
+    if pat[i] == pat[len]:
+      len += 1
+      lps[i] = len
+      i += 1
+    else:
+      if len != 0:
+        len = lps[len - 1]
+      else:
+        lps[i] = 0
+        i += 1
+
+def KMPSearch(pat, txt):
+  m = len(pat)
+  n = len(txt)
+  lps = [0]*m
+  j = 0
+  computeLPS(pat, m, lps)
+  i = 0
+  
+
+txt = "ABABDABACDABABCABAB"
+pat = "ABABCABAB"
+KMPSearch(pat, txt)
