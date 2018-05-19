644979fb7467f48a122f5936c68a19b5451c30b2
Lalit527 <lalit.slg007@gmail.com>
2018-05-19 16:55:38 +0530

    Added DP program for knapsack

--- Sequence2/DP/knapsack.py
+++ Sequence2/DP/knapsack.py
@@ -8,6 +8,18 @@ def knapsack(W, wt, val, n):
   return max(val[n-1] + knapsack(W-wt[n-1], wt, val, n-1), 
               knapsack(W, wt, val, n-1))
 
+def knapsack_memo(W, wt, val, n):
+  k = [[0 for x in range(W+1)] for x in range(n+1)]
+
+  for i in range(n+1):
+    for w in range(W+1):
+      if i == 0 or w == 0:
+        K[i][w] = 0
+      elif wt[i-1] <= w:
+        K[i][w] = max(val[i-1] + K[i-1][w-wt[i+1]], K[i-1][w])
+      else:
+        K[i][w] = K[i-1][w]
+
 val = [60, 100, 120]
 wt = [10, 20, 30]
 W = 50
