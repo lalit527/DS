a492947fe68377a2d2257fa53506390f7b6f1256
Lalit527 <lalit.slg007@gmail.com>
2018-05-22 20:46:58 +0530

    Added AVL tree

--- Sequence2/Trees/avltree.py
+++ Sequence2/Trees/avltree.py
@@ -43,6 +43,9 @@ class AvlTree:
     if balance > 1 and key > node.left.key:
       node.left = self.leftRotate(node.left)
       return self.rightRotate(node)
+
+    if balance < -1 and key > node.right.key:
+      return self.leftRotate(node)
     
     if balance < -1 and key < node.right.key:
       node.right = self.rightRotate(node.right)
@@ -50,32 +53,79 @@ class AvlTree:
 
     return node
 
-def delete(self, root, key):
-  if root is None:
-    return root
-  
-  if root.data < key:
-    root.right = self.delete(root.right, key)
-  elif root.data > key:
-    root.left = self.left(root.left, key)
-  
-  else:
-    if root.right is None:
-      tmp = root.left
-      root = None
-      return tmp
+  def delete(self, root, key):
+    if root is None:
+      return root
     
-    elif root.left is None:
-      tmp = root.right
-      root = None
-      return tmp
-
+    if root.data < key:
+      root.right = self.delete(root.right, key)
+    elif root.data > key:
+      root.left = self.left(root.left, key)
+    
     else:
-      tmp = self.getSuccessor(root.right)
-      root.data = tmp.data
-      self.delete(root.right, tmp.data)
+      if root.right is None:
+        tmp = root.left
+        root = None
+        return tmp
+      
+      elif root.left is None:
+        tmp = root.right
+        root = None
+        return tmp
 
-  if root is None:
+      else:
+        tmp = self.getSuccessor(root.right)
+        root.data = tmp.data
+        self.delete(root.right, tmp.data)
+
+    if root is None:
+      return root
+
+    root.height = 1 + max(self.height(root.left), self.height(root.right))
+
+    balance = self.getBalance(root)
+
+    if balance > 1 and key < root.left.key:
+        return self.rightRotate(root)
+      
+    if balance > 1 and key > root.left.key:
+      root.left = self.leftRotate(root.left)
+      return self.rightRotate(root)
+
+    if balance < -1 and key > root.right.key:
+      return self.leftRotate(root)
+    
+    if balance < -1 and key < root.right.key:
+      root.right = self.rightRotate(root.right)
+      return self.leftRotate(root)
+
     return root
 
-  root.height = 1 + max(self.height(root.left), self.height(root.right))
+
+  def leftRotate(self, root):
+    y = node.right
+    tmp = y.left
+
+    y.left = Node
+    node.right = tmp
+
+    node.height = 1 + max(self.height(node.left), self.height(node.right))
+    y.height = 1 + max(self.height(y.left), self.height(y.right))
+    
+    return y
+
+  def rightRotate(self, node):
+    x = node.left
+    tmp = x.right
+    
+    x.right = node
+    node.left = tmp
+    
+    node.height = 1 + max(self.height(node.left), self.height(node.right))
+    x.height = 1 + max(self.height(x.left), self.height(x.right))
+    
+    return x
+
+
+
+t = AvlTree()
