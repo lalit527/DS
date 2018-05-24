ee55d2df75513bcfa3e97b35790240beeef9c712
Lalit527 <lalit.slg007@gmail.com>
2018-05-24 07:49:18 +0530

    Added Quicksort

--- CLRS/QuickSort/quicksort.py
+++ CLRS/QuickSort/quicksort.py
@@ -1,6 +1,8 @@
+import random
+
 def _quicksort(arr, s, e):
   if s < e:
-    q = partition(arr, s, e)
+    q = hoare_partition(arr, s, e)
     _quicksort(arr, s, q-1)
     _quicksort(arr, q+1, e)
 
@@ -14,9 +16,31 @@ def partition(arr, s, e):
   arr[i + 1], arr[e] = arr[e], arr[i+1]
   return i + 1
 
+def random_partition(arr, s, e):
+  i = random.randint(s, e)
+  arr[i], arr[e] = arr[e], arr[i]
+  return partition(arr, s, e)
+
 def quicksort(arr):
   n = len(arr)
   _quicksort(arr, 0, n-1)
+
+
+def hoare_partition(arr, s, e):
+  x = arr[s]
+  i = s
+  j = e
+  done = False
+  while not done:
+    while j <= e and arr[j] < x:
+      j -= 1
+    while i<= e and arr[i] >= x:
+      i += 1
+    if i < j:
+      arr[i], arr[j] = arr[j], arr[i]
+    else:
+      done = True
+  return j
 
 arr = [ 12, 11, 13, 5, 6, 7]
 quicksort(arr)
