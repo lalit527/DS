b9bf604aa0bba789196d85c4edbefffef37b32a1
Lalit527 <lalit.slg007@gmail.com>
2018-07-09 12:20:57 +0530

    Starter files for Week4 on graph

---
+++ Sequence2/DP/rodcutting.py
@@ -0,0 +1,8 @@
+def cutRod(arr, size):
+  if size <= 0:
+    return 0
+  for i in arr:
+    _max = 
+arr = [1, 5, 8, 9, 10, 17, 17, 20]
+size = len(arr)
+print("Maximum Obtainable Value is", cutRod(arr, size))
