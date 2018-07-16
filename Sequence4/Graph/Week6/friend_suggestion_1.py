77a79b0eb5cf2c8f414e1e61f1750bc9b60e4230
Lalit527 <lalit.slg007@gmail.com>
2018-07-16 13:23:34 +0530

    Added Graph Week6 problem1 and problem2

--- Sequence4/Graph/Week6/friend_suggestion_1.py
+++ Sequence4/Graph/Week6/friend_suggestion_1.py
@@ -3,6 +3,7 @@
 import sys
 import heapq
 import random
+import time
 
 """
 class BiDij:
@@ -51,12 +52,16 @@ class PriorityEntry(object):
 
 
 class Vertex:
-  def __init__(self, data):
+  def __init__(self, data=None):
     self.data = data
     self.connections = {}
 
   def add_neighbours(self, nb, wt):
-    self.connections[nb] = wt
+    try:
+      if self.connections[nb] and self.connections[nb] < wt:
+        return
+    except:
+      self.connections[nb] = wt
 
   def get_connections(self):
     return self.connections
@@ -65,28 +70,27 @@ class Vertex:
     return [(self.data, key.data, value) for key, value in self.connections.items()]
 
   def __str__(self):
-    return str(self.data) + ' connected to: ' + str([{key.data: value} for key, value in self.connections.items()])
+    return ' connected to: ' + str(self.connections)
 
 class DirectedGraph:
-  def __init__(self):
-    self.vertexes = {}
-    self.size = 0
+  def __init__(self, size):
+    self.vertexes = [Vertex()] * (size + 1)
+    self.size = size
 
   def add_vertex(self, data):
-    self.size += 1
     v = Vertex(data)
     self.vertexes[data] = v
 
   def add_edge(self, fr, to, wt):
-    if fr not in self.vertexes:
+    if self.vertexes[fr].data is None:
       self.add_vertex(fr)
-      self.size += 1
-    
-    if to not in self.vertexes:
+
+    if self.vertexes[to].data is None:
       self.add_vertex(to)
-      self.size += 1
+    # self.vertexes[fr].data = fr
+    # self.vertexes[to].data = to
     
-    self.vertexes[fr].add_neighbours(self.vertexes[to], wt)
+    self.vertexes[fr].add_neighbours(to, wt)
 
   def get_vertex(self, v):
     if v in self.vertexes:
@@ -94,84 +98,74 @@ class DirectedGraph:
     return {}
 
   def get_all_vertex(self, v):
-    if v in self.vertexes:
-      return self.vertexes[v].get_all_connections()
+    return self.vertexes[v].get_connections()
 
     
   def all_edges(self):
     result = [value.get_all_connections() for key, value in self.vertexes.items()]
     return [item for sublist in result for item in sublist]
 
-  def transpose(self):
-    g = DirectedGraph()
-    for vertex in self.vertexes:
-      for u, v, w in self.get_all_vertex(vertex):
-        g.add_edge(v, u, w)
-    return g
-    
   def __str__(self):
     result = ""
-    for key, value in self.vertexes.items():
-      result += str(value) + '\n'
+    for key, value in enumerate(self.vertexes):
+      result += str(key) + str(value) + '\n'
     return result
 
 def readl():
     return map(int, sys.stdin.readline().split())
 
-def bidirectional(G, G_r, s, t):
+def bidirectional(G, G_r, size, s, t):
   _max = float('inf')
-  dist = {}
-  dist_r = {}
-  prev = {}
-  prev_r = {}
+  # dist = {}
+  # dist_r = {}
+  # prev = {}
+  # prev_r = {}
   H = []
   H_r = []
-  for u in G.vertexes:
-    dist[u] = _max
-    dist_r[u] = _max
-    prev[u] = None
-    prev_r[u] = None
-  
-  dist[s] = 0
-  dist_r[t] = 0
+  dist = [_max] * (size + 1) 
+  dist_r = [_max] * (size + 1)
+  prev = []
+  prev_r = [] 
+  seen = [False] * (size + 1)
+  seen_r = [False] * (size + 1)
+
   proc = []
   proc_r = []
-  heapq.heappush(H, PriorityEntry(0, s))
-  heapq.heappush(H_r, PriorityEntry(0, t))
+ 
+  dist[s] = 0
+  dist_r[t] = 0
+  heapq.heappush(H, (0, s))
+  heapq.heappush(H_r, (0, t))
   while len(H) > 0 and len(H_r) > 0:
     v = heapq.heappop(H)
-    process(v.data, G, dist, prev, proc, H)
-    if v.data in proc_r:
+    process(v[1], G, dist, prev, proc, H, seen)
+    if seen_r[v[1]]:
       return shortestpath(s, dist, prev, proc, t, dist_r, prev_r, proc_r)
     
     v_r = heapq.heappop(H_r)
-    process(v_r.data, G_r, dist_r, prev_r, proc_r, H_r)
-    if v_r.data in proc:
+    process(v_r[1], G_r, dist_r, prev_r, proc_r, H_r, seen_r)
+    if seen[v_r[1]]:
       return shortestpath(s, dist, prev, proc, t, dist_r, prev_r, proc_r)
    
-  return (-1, [])
+  return -1
 
-def process(u, G, dist, prev, proc, H):
-  D = G.get_all_vertex(u)
-  if D is not None:
-    for u, v, w in D:
-      relax(u, v, w, dist, prev, H)
-    proc.append(u)
+# def process(u, G, dist, prev, proc, H):
+#   D = G.get_all_vertex(u)
+#   if D is not None:
+#     for v, w in D.items():
+#       relax(u, v, w, dist, prev, H)
+#     proc[u] = True
+
+def process(u, G, dist, prev, proc, H, seen):
+  for v, w in G.get_all_vertex(u).items():
+    relax(u, v, w, dist, prev, H)
+  proc.append(u)
+  seen[u] = True
 
 def relax(u, v, w, dist, prev, H):
   if dist[v] > dist[u] + w:
     dist[v] = dist[u] + w
-    prev[v] = u
-    d = PriorityEntry(dist[v], v)
-    try:
-      i = H.index(d)
-    except ValueError:
-      i = -1
-    if i > -1:
-      H[i] = d
-      heapq.heapify(H)
-    else:
-      heapq.heappush(H, d)
+    heapq.heappush(H, (dist[v], v))
 
 def shortestpath(s, dist, prev, proc, t, dist_r, prev_r, proc_r):
   distance = float('inf')
@@ -180,62 +174,77 @@ def shortestpath(s, dist, prev, proc, t, dist_r, prev_r, proc_r):
     if dist[u] + dist_r[u] < distance:
       ubest = u
       distance = dist[u] + dist_r[u]
-  path = []
-  last = ubest
-  while last != s:
-    path.append(last)
-    last = prev[last]
-  path = list(reversed(path))
-  last = ubest
-  while last != t:
-    last = prev_r[last]
-    path.append(last)
-  return (distance, path)
+  return distance
+  # last = ubest
+  # while last != s:
+  #   path.append(last)
+  #   last = prev[last]
+  # path = list(reversed(path))
+  # last = ubest
+  # while last != t:
+  #   last = prev_r[last]
+  #   path.append(last)
+  
 
 def main():
-  n = 10
-  m = 10
-  G = DirectedGraph()
-  for i in range(1, n + 1):
-    G.add_vertex(i)
+  start = time.time()
+  n = 100000
+  m = 200000
+  G = DirectedGraph(n)
+  G_r = DirectedGraph(n)
+  # for i in range(1, n + 1):
+  #   G.add_vertex(i)
   for _ in range(m):
     u = random.randint(1, n)
     v = random.randint(1, n)
-    c = random.randint(1, 20)
+    c = random.randint(1, 1000)
     G.add_edge(u, v, c)
-  t = 10
-  G_r = G.transpose()
-  print(G)
-  print(G_r)
+    G_r.add_edge(v, u, c)
+  start2 = time.time()
+  print('G_Allc', start2 - start)
+  t = 10000
+  # print(G)
+  # print(G_r)
+  
   for _ in range(t):
     s = random.randint(1, n)
     t = random.randint(1, n)
-    print(bidirectional(G, G_r, s, t)[0])
+    if s == t:
+      print(0)
+    else:
+      print(bidirectional(G, G_r, n, s, t))
+
+  end = time.time()
+  print(end - start2)
+
 
 if __name__ == '__main__':
-  main()
-  """
+  # main()
+  
     n,m = readl()
     # adj = [[[] for _ in range(n)], [[] for _ in range(n)]]
     # cost = [[[] for _ in range(n)], [[] for _ in range(n)]]
-    G = DirectedGraph()
-    for i in range(1, n + 1):
-      G.add_vertex(i)
+    G = DirectedGraph(n)
+    G_r = DirectedGraph(n)
+    # for i in range(1, n + 1):
+    #   G.add_vertex(i)
     for e in range(m):
         u,v,c = readl()
         G.add_edge(u, v, c)
+        G_r.add_edge(v, u, c)
         # adj[0][u-1].append(v-1)
         # cost[0][u-1].append(c)
         # adj[1][v-1].append(u-1)
         # cost[1][v-1].append(c)
+
     t, = readl()
-    G_r = G.transpose()
-    print(G)
-    print(G_r)
     # bidij = BiDij(n)
     for i in range(t):
         s, t = readl()
-        print(bidirectional(G, G_r, s, t)[0])
+        if s == t:
+          print(0)
+        else:
+          print(bidirectional(G, G_r, n, s, t))
         # count += 1
         # if count == 4:
         #   print(G)
@@ -245,5 +254,3 @@ if __name__ == '__main__':
 
         # print(s, t)
         # print(bidij.query(adj, cost, s-1, t-1))
-
-    """
