b0f598e14baaf8a0de247a4dabd47488b2922de4
Lalit527 <lalit.slg007@gmail.com>
2018-07-27 12:10:24 +0530

    Added suffix Array

--- Sequence4/String/Week4/class/suffix-array.py
+++ Sequence4/String/Week4/class/suffix-array.py
@@ -1,50 +1,52 @@
 def sort_characters(S):
   n = len(S)
-  sorted_char = sorted(set(S))
+  sort_char_set = sorted(set(S))
   order = [None] * n
-  count = [S.count(c) for c in sorted_char]
+  count = [S.count(c) for c in sort_char_set]
   for i in range(1, len(count)):
     count[i] += count[i - 1]
-  for i in range(n - 1, -1, -1):
-    index = sorted_char.index(S[i])
+  for i in range(n-1, -1, -1):
+    index = sort_char_set.index(S[i])
     count[index] -= 1
     order[count[index]] = i
   return order
-
+  
 def compute_char_klass(S, order):
   n = len(S)
   klass = [None] * n
   klass[order[0]] = 0
   for i in range(1, n):
     if S[order[i]] != S[order[i - 1]]:
-      klass[order[i]] = klass[order[i-1]] + 1
+      klass[order[i]] = klass[order[i - 1]] + 1
     else:
-      klass[order[i]] = klass[order[i-1]]
+      klass[order[i]] = klass[order[i - 1]]
   return klass
 
 def sort_doubled(S, L, order, klass):
   n = len(S)
   count = [0] * n
-  newOrder = [None] * n
+  new_order = [None] * n
   for i in range(n):
-    count[klass[i]] = count[klass[i]] + 1
+    count[klass[i]] += 1
+  
   for j in range(1, n):
-    count[j] = count[j] + count[j - 1]
+    count[j] += count[j - 1]
+  
   for i in range(n-1, -1, -1):
-    start = (order[i] - L + n) % (n)
+    start = (order[i] - L + n) % n
     cl = klass[start]
     count[cl] = count[cl] - 1
-    newOrder[count[cl]] = start
-  return newOrder
+    new_order[count[cl]] = start
+  return new_order
 
-def update_klass(newOrder, klass, L):
-  n = len(newOrder)
+def update_klass(new_order, klass, L):
+  n = len(new_order)
   new_klass = [None] * n
-  new_klass[newOrder[0]] = 0
+  new_klass[new_order[0]] = 0
   for i in range(1, n):
-    cur = newOrder[i]
-    prev = newOrder[i - 1]
-    mid = (cur + L) 
+    cur = new_order[i]
+    prev = new_order[i - 1]
+    mid = (cur + L)
     mid_prev = (prev + L) % n
     if klass[cur] != klass[prev] or klass[mid] != klass[mid_prev]:
       new_klass[cur] = new_klass[prev] + 1
@@ -54,7 +56,9 @@ def update_klass(newOrder, klass, L):
 
 def build_suffix_array(S):
   order = sort_characters(S)
+  print(order)
   klass = compute_char_klass(S, order)
+  print(klass)
   L = 1
   while L < len(S):
     order = sort_doubled(S, L, order, klass)
