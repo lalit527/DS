d04d5d8c3868eb44ee40cf06a4b7e748afece031
Lalit527 <lalit.slg007@gmail.com>
2018-08-17 18:38:54 +0530

    Added Week2

--- Sequence4/Advance-Algorithm/Week2/22diet.py
+++ Sequence4/Advance-Algorithm/Week2/22diet.py
@@ -1,9 +1,20 @@
 from sys import stdin
 from bitarray import bitarray
-EPS = 1e-6
-PRECISION = 20
-INF = float('-inf')
+from functools import reduce
+import sys
 
+EPS = 1e-3
+INF = 1.0e+9
+
+class Equation:
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
+
+class Position:
+  def __init__(self, column, row):
+    self.column = column
+    self.row = row
 
 def ReadEquation():
     n, m = list(map(int, stdin.readline().split()))
@@ -14,6 +25,92 @@ def ReadEquation():
     c = list(map(int, stdin.readline().split()))
     # print(A, '--', b, '--', c)
     return (n, m, A, b, c)
+
+def SelectPivotElement(a, used_rows, used_columns):
+    # This algorithm selects the first free element.
+    # You'll need to improve it to pass the problem.
+    pivot_element = Position(0, 0)
+    while used_rows[pivot_element.row]:
+        pivot_element.row += 1
+    while used_columns[pivot_element.column]:
+        pivot_element.column += 1
+    
+    _max = 0.0
+    size = len(a)
+    for i in range(pivot_element.row, size):
+      if abs(_max) - abs(a[i][pivot_element.column]) < EPS:
+        _max = abs(a[i][pivot_element.column])
+        pivot_element.row = i
+    return pivot_element
+
+def SwapLines(a, b, used_rows, pivot_element):
+    a[pivot_element.column], a[pivot_element.row] = a[pivot_element.row], a[pivot_element.column]
+    b[pivot_element.column], b[pivot_element.row] = b[pivot_element.row], b[pivot_element.column]
+    used_rows[pivot_element.column], used_rows[pivot_element.row] = used_rows[pivot_element.row], used_rows[pivot_element.column]
+    pivot_element.row = pivot_element.column
+
+def scale_pivot(a, b, pivot_element):
+  size = len(a)
+  divisor = a[pivot_element.row][pivot_element.column]
+  for j in range(pivot_element.column, size):
+    a[pivot_element.row][j] /= divisor
+  
+  b[pivot_element.row] /= divisor
+
+
+def ProcessPivotElement(a, b, pivot_element):
+    # Write your code here
+    size = len(a)
+    multiple = 0.0
+    scale_pivot(a, b, pivot_element)
+    print('SP', b, 'PE', pivot_element.row, pivot_element.column)
+    for i in range(pivot_element.row + 1, size):
+      multiple = a[i][pivot_element.column]
+      for j in range(pivot_element.column, size):
+        a[i][j] -= (a[pivot_element.row][j] * multiple)
+      print('Check1', multiple, b[i], pivot_element.row, pivot_element.column, b[pivot_element.row], multiple) 
+      b[i] -= (b[pivot_element.row] * multiple)
+    print('SPE', b)
+    
+
+
+def MarkPivotElementUsed(pivot_element, used_rows, used_columns):
+    used_rows[pivot_element.row] = True
+    used_columns[pivot_element.column] = True
+
+def SolveEquation(equation):
+    print('SE', equation.a, equation.b)
+    a = equation.a
+    b = equation.b
+    size = len(a)
+    if size <= 0:
+      return {}
+
+    used_columns = [False] * size
+    used_rows = [False] * size
+    for step in range(size):
+        pivot_element = SelectPivotElement(a, used_rows, used_columns)
+        if a[pivot_element.row][pivot_element.column] == 0:
+          break
+        SwapLines(a, b, used_rows, pivot_element)
+        ProcessPivotElement(a, b, pivot_element)
+        MarkPivotElementUsed(pivot_element, used_rows, used_columns)
+    code = back_substitution(a, b)
+    
+    return (code, b)
+
+def back_substitution(a, b):
+  size = len(a)
+  # print('BEGIN', a, b)
+  for i in range(size - 1, -1, -1):
+    v = b[i]
+    for j in range(0, i):
+      # print('bj', b[j], 'a', a[j][i], 'v', v)
+      b[j] -= a[j][i] * v
+      a[j][i] = 0
+  # print('END', a, b)
+  return 0
+
 
 def prepare(n, m, A, b):
   while n < m:
@@ -45,15 +142,13 @@ def get_subsets(set, m):
     s_n = len(set)
     for j in range(s_n):
       if bset[32 - 1 - (s_n - 1 - j)] == '1':
-        print('Fuck', bset[s_n - 1 - j])
         subset.append(set[j])
         cnt += 1
         if cnt > m:
           # print(i, j, cnt, m)
           break
     if cnt == m:
-      subsets.append(subset[:])
-  print(subsets)
+      subsets.append(subset)
   return subsets
       
 
@@ -61,14 +156,71 @@ def solve_all_equations(m, A, b):
   solutions = []
   nums = [i for i in range(len(A))]
   subsets = get_subsets(nums, m)
-
+  if len(A) == 1:
+    subsets.append[0]
+  
+  for sub in subsets:
+    mat = []
+    col = []
+    for i in sub:
+      mat.append(A[i])
+      col.append(b[i])
+    eq = Equation(mat, col)
+    code_sol = SolveEquation(eq)
+    # print('code_sol', code_sol)
+    if code_sol[0] == 0 and len(code_sol[1]) > 0:
+      solutions.append(code_sol[1])    
+  return solutions
 
 def solve_diet(n, m, A, b, c):
   n = prepare(n, m, A, b)
   solutions = solve_all_equations(m, A, b)
-  pass
+  print(solutions)
+  if len(solutions) == 0:
+    print("1")
+    return (-1, {})
+  
+  sol_index = -1
+  largest_pleasure = float('-inf')
+
+  for i in range(len(solutions)):
+    # print('i', solutions[i])
+    sol = solutions[i]
+    satisfied = True
+    for j in range(n):
+      constraint = b[j]
+      sum = 0.0
+
+      for k in range(m):
+        sum += A[j][k] * sol[k]
+      print('SC', sum, constraint)
+      if sum - constraint > EPS:
+        satisfied = False
+        break
+    pleasure = 0.0
+    for k in range(m):
+      # print('k', sol[k], c[k])
+      pleasure += sol[k] * c[k]
+    print('F', pleasure, largest_pleasure)
+    if satisfied and pleasure > largest_pleasure:
+      largest_pleasure = pleasure
+      sol_index = i
+    
+  if sol_index == -1:
+    print("2")
+    return (-1, {})
+  
+  
+
+  sol = solutions[sol_index]
+  print(reduce((lambda x, y: x + y), sol))
+  if reduce((lambda x, y: x + y), sol) + EPS >= INF:
+    print("fuck")
+  print(sol, 'ok')
+
 
 if __name__ == "__main__":
   n, m, A, b, c = ReadEquation()
-  solve_diet(n, m, A, b, c)
+  sol = solve_diet(n, m, A, b, c)
+  print(sol)
 
--- Sequence4/Advance-Algorithm/Week2/2diet-2.py
+++ Sequence4/Advance-Algorithm/Week2/2diet-2.py
@@ -1,68 +1,65 @@
 # python3
 from sys import stdin
+import copy
+import numpy as np
 
-EPS = 1e-6
-PRECISION = 20
+VeryBigNumber = 1e9
+
 
 class Equation:
-    def __init__(self, a, b):
+    def __init__( self, a, b ):
         self.a = a
         self.b = b
 
 class Position:
-    def __init__(self, column, row):
+    def __init__( self, column, row ):
         self.column = column
         self.row = row
 
-def SelectPivotElement(a, used_rows, used_columns):
-    # This algorithm selects the first free element.
-    # You'll need to improve it to pass the problem.
+
+def Prime_element( a, used_rows, used_columns ):
+    mm = len(a)
     pivot_element = Position(0, 0)
     while used_rows[pivot_element.row]:
         pivot_element.row += 1
     while used_columns[pivot_element.column]:
         pivot_element.column += 1
-    
-    _max = 0.0
-    size = len(a)
-    for i in range(pivot_element.row, size):
-      if abs(a[i][pivot_element.column]) > abs(_max):
-        _max = abs(a[i][pivot_element.column])
-        pivot_element.row = i
-    return pivot_element
+    while 0 == a[pivot_element.row][pivot_element.column] or used_rows[pivot_element.row]:
+        pivot_element.row += 1
+        if pivot_element.row > mm - 1:
+            return False, None
+    return True, pivot_element
 
-def SwapLines(a, b, used_rows, pivot_element):
+
+def SwapLines( a, b, used_rows, pivot_element ):
     a[pivot_element.column], a[pivot_element.row] = a[pivot_element.row], a[pivot_element.column]
     b[pivot_element.column], b[pivot_element.row] = b[pivot_element.row], b[pivot_element.column]
-    used_rows[pivot_element.column], used_rows[pivot_element.row] = used_rows[pivot_element.row], used_rows[pivot_element.column]
+    used_rows[pivot_element.column], used_rows[pivot_element.row] = used_rows[pivot_element.row], used_rows[
+        pivot_element.column]
     pivot_element.row = pivot_element.column
 
-def scale_pivot(a, b, pivot_element):
-  size = len(a)
-  divisor = a[pivot_element.row][pivot_element.column]
-  for j in range(pivot_element.column, size):
-    a[pivot_element.row][j] /= divisor
-  
-  b[pivot_element.row] /= divisor
+
+def ProcessPivotElement( a, b, pivot_element ):
+    n = len(a)
+    mm = len(a[pivot_element.row])
+    scale = a[pivot_element.row][pivot_element.column]
+    for j in range(mm):
+        a[pivot_element.row][j] /= scale
+    b[pivot_element.row] /= scale
+    for i in range(n):
+        if i != pivot_element.row:
+            scale = a[i][pivot_element.column]
+            for j in range(pivot_element.column, n):
+                a[i][j] -= a[pivot_element.row][j] * scale
+            b[i] -= b[pivot_element.row] * scale
 
 
-def ProcessPivotElement(a, b, pivot_element):
-    # Write your code here
-    size = len(a)
-    multiple = 0.0
-    scale_pivot(a, b, pivot_element)
-    for i in range(pivot_element.row + 1, size):
-      multiple = a[i][pivot_element.column]
-      for j in range(pivot_element.column, size):
-        a[i][j] -= (a[pivot_element.row][j] * multiple)
-      b[i] -= b[pivot_element.row] * multiple
-
-
-def MarkPivotElementUsed(pivot_element, used_rows, used_columns):
+def MarkPivotElementUsed( pivot_element, used_rows, used_columns ):
     used_rows[pivot_element.row] = True
     used_columns[pivot_element.column] = True
 
-def SolveEquation(equation):
+
+def SolveEquation( equation ):
     a = equation.a
     b = equation.b
     size = len(a)
@@ -70,60 +67,103 @@ def SolveEquation(equation):
     used_columns = [False] * size
     used_rows = [False] * size
     for step in range(size):
-        pivot_element = SelectPivotElement(a, used_rows, used_columns)
+        solved, pivot_element = Prime_element(a, used_rows, used_columns)
+        if not solved:
+            return False, None
         SwapLines(a, b, used_rows, pivot_element)
         ProcessPivotElement(a, b, pivot_element)
         MarkPivotElementUsed(pivot_element, used_rows, used_columns)
 
-    back_substitution(a, b)
-
-    return b
-
-def back_substitution(a, b):
-  size = len(a)
-  for i in range(size - 1, -1, -1):
-    v = b[i]
-    for j in range(i+1, size):
-      b[j] -= a[j][i] * v
-      a[j][i] = 0
+    return True, b
 
 
+def addEquations( n, mm, A, b, Big_number ):
+    for i in range(mm):
+        e = [0.0] * mm
+        e[i] = -1.0
+        A.append(e)
+        b.append(0.0)
+    A.append([1.0] * mm)
+    b.append(Big_number)
 
-def PrintColumn(column):
-    size = len(column)
-    for row in range(size):
-        print("%.20lf" % column[row])
 
-class LinearEquationSolver:
-    def __init__(self, A, b, c):
-        self.n = A.length
-        self.m = c.length
-        self.A = A
-        self.b = b
-        self.c = c
-        total = n + m + 1
-    
-    def compute(self, total):
-        arr = [None] * total
-        for i in range(total):
-            arr[i] = i
-        gen_process_combinators(arr, total, )
-
-def ReadEquation():
-    n, m = list(map(int, stdin.readline().split()))
-    A = []
+def checkResult( n, mm, A, b, c, result, lastEquation, ans, bestScore ):
+    for r in result:
+        if r < -1e-3:
+            return False, ans, bestScore
     for i in range(n):
-      A += [list(map(int, stdin.readline().split()))]
-    b = list(map(int, stdin.readline().split()))
-    c = list(map(int, stdin.readline().split()))
-    print(A)
-    print(b, c)
+        r = 0.0
+        for j in range(mm):
+            r += A[i][j] * result[j]
+        if r > b[i] + 1e-3:
+            return False, ans, bestScore
+    score = 0.0
+    for j in range(mm):
+        score += c[j] * result[j]
+    if score <= bestScore:
+        return False, ans, bestScore
+    else:
+        if lastEquation:
+            return True, 1, score
+        else:
+            return True, 0, score
 
 
-if __name__ == "__main__":
-    equation = ReadEquation()
-    # print(equation.a)
-    # print(equation.b)
-    # solution = SolveEquation(equation)
-    # PrintColumn(solution)
-    # exit(0)
+def solve_diet_problem( n, mm, A, b, c, Big_number=VeryBigNumber ):
+    addEquations(n, mm, A, b, Big_number)
+    # print(A, b)
+    l = n + mm + 1
+    ans = -1
+    bestScore = -float('inf')
+    bestResult = None
+    for x in range(2 ** l):
+        usedIndex = [i for i in range(l) if ((x / 2 ** i) % 2) // 1 == 1]
+        if len(usedIndex) != mm:
+            continue
+        lastEquation = False
+        if usedIndex[-1] == l - 1:
+            lastEquation = True
+        As = [A[i] for i in usedIndex]
+        bs = [b[i] for i in usedIndex]
+        # print(As, bs)
+        solved, result = SolveEquation(copy.deepcopy(Equation(As, bs)))
+        # print(As, bs, result)
+        if solved:
+            isAccepted, ans, bestScore = checkResult(n, mm, A, b, c, result, lastEquation, ans, bestScore)
+            if isAccepted:
+                bestResult = result
+    # print(A)
+    return [ans, bestResult]
+
+
+def solve_diet_problem0( n, mm, A, b, c ):
+    # addEquations(n, mm, A, b, VeryBigNumber)
+    res = linprog(-np.array(c), A, b)
+    if 3 == res.status:
+        ans = 1
+        x = None
+    elif 0 == res.status:
+        ans = 0
+        x = list(res.x)
+    else:
+        ans = -1
+        x = None
+    return ans, x
+
+
+n, mm = list(map(int, stdin.readline().split()))
+A = []
+for i in range(n):
+    A += [list(map(int, stdin.readline().split()))]
+b = list(map(int, stdin.readline().split()))
+c = list(map(int, stdin.readline().split()))
+
+anst, ansx = solve_diet_problem(n, mm, A, b, c)
+
+if anst == -1:
+    print("No solution")
+if anst == 0:
+    print("Bounded solution")
+    print(' '.join(list(map(lambda x: '%.18f' % x, ansx))))
+if anst == 1:
+    print("Infinity")
