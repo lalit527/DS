72925edee8404e70c6bdc088eee540a502f488af
Lalit527 <lalit.slg007@gmail.com>
2018-09-26 21:39:04 +0530

    Added rod cutting

--- Sequence5/CTCI/8-Rec-DP/Others/rod_cutting.py
+++ Sequence5/CTCI/8-Rec-DP/Others/rod_cutting.py
@@ -18,3 +18,30 @@ def rod_cutting_dp(price, n):
 price = [1, 5, 8, 9, 10, 17, 17, 20]
 
 print('final', rod_cutting_dp(price, len(price)))
+
+"""
+def rod_cut(rods, n):
+    if n <= 0:
+      return 0
+    max_profit = float('-inf')
+    for i in range(n):
+      max_profit = max(max_profit, rods[i] + rod_cut(rods, n - 1 - i))
+    return max_profit
+
+def rod_cut_main(rods):
+  n = len(rods)
+  return rod_cut(rods, n)
+
+def rod_cut_dp(rods):
+  n = len(rods)
+  T = [0 for i in range(n + 1)]
+  for i in range(1, n + 1):
+    max_profit = float('-inf')
+    for j in range(i):
+      max_profit = max(max_profit, rods[j] + T[i - j - 1])
+    T[i] = max_profit
+  return T[n]
+    
+
+rods = [2, 4, 6, 8, 10]
+"""
