7c7624704633c4a17041cf0bcc866aded84b4cea
Lalit527 <lalit.slg007@gmail.com>
2018-09-27 20:23:05 +0530

    Added max sum in a rectangle

---
+++ Sequence5/CTCI/8-Rec-DP/Others/max_sum_rectangle.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+
+Result = namedtuple("Result","maxSum leftBound rightBound upBound lowBound")
+
+KadanesResult = namedtuple("KadanesResult","maxSum start end")
+
+def kadanes(temp):
+  _max = 0
+  max_start = -1
+  max_end = -1
+  current_start = 0
+  max_so_far = 0
+  for i in range(len(temp)):
+    max_so_far += temp[i]
+    if max_so_far < 0:
+      max_so_far = 0
+      current_start = i + 1
+    if max_so_far > _max:
+      max_start = current_start
+      max_end = i
+      max = max_so_far
+  return KadanesResult(max, max_start, max_end)
+
+
+def max_sum_rectangle(rectangle):
+  rows = len(rectangle)
+  cols = len(rectangle[0])
+  result = Result(float('-inf'), -1, -1, -1, -1)
+  for left in range(cols):
+    temp = [0 for _ in range(rows)]
+    for right in range(left, cols):
+      for i in range(rows):
+        temp[i] += rectangle[i][right]
+      
+      kadane_result = kadanes(temp)
+      if kadane_result.maxSum > result.maxSum:
+        result = Result(kadane_result.maxSum, left, right, kadane_result.start, kadane_result.end)
+  return result
+
+M = [
+  [2, 1, -3, -4, 5], 
+  [0, 6, 3, 4, 1], 
+  [2, -2, -1, 4, -5], 
+  [-3, 3, 1, 0, 3]
+]
+
+result = max_sum_rectangle(M)
+print(result)
