267b4a847066d05ca8c515d972a4453a53f6140b
Lalit527 <lalit.slg007@gmail.com>
2018-10-04 20:10:19 +0530

    Added strongly connected graph

--- Sequence4/Graph/Week2/class/Rev/strongly_connected.py
+++ Sequence4/Graph/Week2/class/Rev/strongly_connected.py
@@ -5,9 +5,27 @@ from toposort import topo_sort
 def invert_graph(G):
   _G = DirectedGraph()
   for u in G.all_vertexes():
-    for v, w in G.get_vertex(u):
+    connections = [(u.data, v) for u, v in G.get_vertex(u).items()]
+    for v, w in connections:
       _G.add_edge(v, u, w)
   return _G
+
+def dfs_rec(G, start, visited):
+  visited.add(start)
+  connections = set([(u.data, v) for u, v in G.get_vertex(start).items()])
+  for v, w in connections - visited:
+    dfs_rec(G, v, visited)
+
+
+def strongly_connected(G):
+  stack = topo_sort(G)
+  G_t = invert_graph(G)
+  visited = set()
+  while len(stack) > 0:
+    v = stack.pop()
+    if v not in visited:
+      dfs_rec(G_t, v, visited)
+
 
 def main():
   G = DirectedGraph()
@@ -20,7 +38,7 @@ def main():
   G.add_edge('B', 'C')
   G.add_edge('B', 'D')
   G.add_edge('A', 'E')
-  print(invert_graph(G))
+  print(strongly_connected(G))
 
 if __name__ == "__main__":
   main()
