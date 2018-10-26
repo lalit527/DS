a0a8e2034b6a51a5174de3ff15f5886afffd0e15
Lalit527 <lalit.slg007@gmail.com>
2018-10-26 12:44:58 +0530

    Added next greater number

--- Sequence7/codechef/3-stack_queue/next_great.py
+++ Sequence7/codechef/3-stack_queue/next_great.py
@@ -4,14 +4,34 @@ from collections import deque
 def next_greater(n, W):
   S = deque()
   n = len(W) - 1
-  S.append(W[n])
-  for i in W[n - 1::-1]:
-    if S[-1] > i:
+  S.append(n)
+  n -= 1
+  while n >= 0:
+    top = S[-1]
+    if W[top] > W[n]:
       break
     else:
-      S.append(i)
-  print(S)
-   
+      S.append(n)
+    n -= 1
+  if n <= 0:
+    return -1
+  top = S.pop()
+  W[top], W[n] = W[n], W[top]
+  if len(S) > 0:
+    peek = S[0]
+    while len(S) > 1:
+      front = S.popleft()
+      rear = S.pop()
+      W[rear], W[front] = W[front], W[rear]
+    if W[top] > W[-1]:
+      tmp = W[top]
+      i = top + 1
+      while i < len(W):
+        W[i - 1] = W[i]
+        i += 1 
+    W[-1] = tmp
+  return ''.join(map(lambda x: str(x), W))
+  
 
 def main():
   num_cases = int(sys.stdin.readline().strip())
