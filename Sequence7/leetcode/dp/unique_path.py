e3d713f630d406ca71cd09ae2e61821746ad4ee6
lalit527 <lalit.slg007@gmail.com>
2018-11-22 14:52:38 +0530

    Python Decorators'

--- Sequence7/leetcode/dp/unique_path.py
+++ Sequence7/leetcode/dp/unique_path.py
@@ -25,6 +25,39 @@ def _solve_maze(maze, result, cur_x, cur_y, dest_x, dest_y):
     result[cur_x][cur_y] = 0
     return False
 
+# count the number of paths in a maze
+def countPaths(maze):
+  n = len(maze)
+  m = len(maze[0])
+  if maze[0][0] == -1:
+    return 0
+  
+  for i in range(n):
+    if maze[i][0] == 0:
+      maze[i][0] = 1
+    else:
+      break
+  
+  for j in range(m):
+    if maze[0][j] == 0:
+      maze[0][j] = 1
+    else:
+      break
+
+  for i in range(1, n):
+    for j in range(1, m):
+      if maze[i][j] == -1:
+        continue
+      if maze[i - 1][j] > 0:
+        maze[i][j] = (maze[i][j] + maze[i - 1][j])
+      
+      if maze[i][j - 1] > 0:
+        maze[i][j] = (maze[i][j] + maze[i][j - 1])
+  print(maze)
+  return maze[n - 1][m - 1]
+  
+
+
 
 if __name__ == "__main__":
     maze = [ [1, 0, 0, 0], 
@@ -32,3 +65,11 @@ if __name__ == "__main__":
              [0, 1, 0, 0], 
              [1, 1, 1, 1] ] 
     print(solve_maze(maze))
+
+    maze2 = [
+              [0, 0, 0, 0], 
+              [0, -1, 0, 0], 
+              [-1, 0, 0, 0], 
+              [0, 0, 0, 0]
+            ]
+    print(countPaths(maze2))
