d50828d3c8eb3e626772d58f35269b185181c632
lalit527 <lalit.slg007@gmail.com>
2018-12-14 14:44:29 +0530

    Added 3 way partition

--- Sequence7/geeks/array/4_seg_array.py
+++ Sequence7/geeks/array/4_seg_array.py
@@ -3,16 +3,39 @@ from collections import defaultdict
 
 def seprate_values_01(arr):
   n = len(arr)
-  i = j = 0
-  while i < n or j < n:
-    while arr[i] == 0:
+  i = 0
+  j = 1
+  while i < n and j < n:
+    while i < n and arr[i] == 0:
       i +=1
-    while arr[j] == 1:
+    while j < n and arr[j] == 1:
       j += 1
     if i < n and j < n:
       arr[i], arr[j] = arr[j], arr[i]
-  print(arr)
+  return arr
 
+def partition_3way(arr):
+  n = len(arr)
+  _partition_3way(arr, 0, n - 1)
+
+def _partition_3way(arr, l, r):
+  pivot = 1
+  i = l
+  lt = l
+  gt = r
+  while i <= gt:
+    if arr[i] < pivot:
+      arr[lt], arr[i] = arr[i], arr[lt]
+      lt += 1
+      i += 1
+    elif arr[i] > pivot:
+      arr[i], arr[gt] = arr[gt], arr[i]
+      gt -= 1
+    else:
+      i += 1
+  print(pivot)
+  print(lt, gt)
+  print(arr)
 
 def main():
   n = int(sys.stdin.readline())
@@ -23,7 +46,7 @@ def main():
     tmp.extend(sys.stdin.readline().split())
     tmp = list(map(lambda x: int(x), tmp))
     data.append(tmp)
-    print(seprate_values_01(tmp))
+    print(partition_3way(tmp))
   
 if __name__ == "__main__":
   main()
